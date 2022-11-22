e9dfa2325df159a027cac4f3e6dcf321f82b6685
hoangkhoa181098 <hoangkhoa181098@gmail.com>
2022-11-22 17:04:59 +0700

    'header'

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Carousel from './Components/Carousel/Carousel';
 import Film from './Components/Flim/Film';
+import Header from './Components/Header';
 import Promotion from './Components/Promotion/Promotion';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Carousel/>
       <Film/>
       <Promotion/>
